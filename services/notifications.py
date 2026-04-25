b11e9044eee051ff6f1a20a085785dc2897504ea
Moizuddin Siddiqui <moizuddinsiddiqui26@gmail.com>
2026-04-26 00:24:31 +0530

    Create notifications.py

---
+++ services/notifications.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def push_notification(message, type="info"):
+
+    if type == "success":
+        st.success(message)
+    elif type == "warning":
+        st.warning(message)
+    elif type == "error":
+        st.error(message)
+    else:
+        st.info(message)
